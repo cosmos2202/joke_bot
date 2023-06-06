6452dc305e97108568e7d262749fbb89bd89848f
cosmos <cosmos.1985@mail.ru>
2023-06-06 09:15:09 +0300

    fix

--- bot.js
+++ bot.js
@@ -1,9 +1,12 @@
 const { Client, GatewayIntentBits } = require('discord.js');
-const bot = new Client({ intents: [
-  GatewayIntentBits.Guilds,
-  GatewayIntentBits.GuildMessages,
-  GatewayIntentBits.MessageContent,
-  GatewayIntentBits.GuildMembers,] });
+const bot = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers
+  ]
+});
 const fetch = require('node-fetch');
 const config = require('./config.json');
 const token = config.token;
@@ -13,20 +16,47 @@ const jokesAPI = 'https://official-joke-api.appspot.com/random_joke';
 
 bot.on('ready', () => {
   console.log(`Successfully launched -> ${bot.user.tag}!`);
+  bot.user.setPresence({
+    status: 'online',
+    activity: {
+      name: '-help',
+      type: 'PLAYING'
+    }
+  });
 });
 
-bot.on('messageCreate', async msg => { console.log('joke')
+bot.on('messageCreate', async msg => {
   if (msg.content.startsWith(prefix)) {
-    const args = msg.content.slice(prefix.length).trim().split(/ +/g);
+    const args = msg.content
+      .slice(prefix.length)
+      .trim()
+      .split(/ +/g);
     const command = args.shift().toLowerCase();
+
     switch (command) {
       case 'help':
-        msg.channel.send(`A bot telling jokes, enter the command '-joke'`);
+        msg.channel.send(`A bot telling jokes, enter the command '-joke' <-> Либо просто '-анекдот'`);
         break;
       case 'joke':
-        const response = await fetch(jokesAPI);
+        var response = await fetch(jokesAPI);
         const joke = await response.json();
         msg.channel.send(`**${joke.setup}**\n${joke.punchline}`);
+        break;
+      case 'анекдот':
+        var response = await fetch('https://www.anekdot.ru/random/anekdot/');
+        const html = await response.text();
+        const regexp = /<div class="text">(.+)<\/div>/s;
+        const match = html.match(regexp);
+        if (match) {
+          const anekdot = match[1]
+            .replace(/<br>/g, '\n')
+            .replace(/&quot;/g, '"')
+            .replace(/&lt;/g, '<')
+            .replace(/&gt;/g, '>');
+          msg.channel.send(anekdot);
+        } else {
+          msg.channel.send('Не удалось получить анекдот :(');
+        }
         break;
     }
   }
